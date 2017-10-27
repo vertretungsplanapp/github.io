6c2297c43e96d1f5763bd67d706b12d893050dcd
Lennart <lennart2@lvideos.de>
2017-10-28 00:21:44 +0200

    Nightly 17w43_6a

--- out/production/VertretungsplanApp3/VertretungsplanApp3.js
+++ out/production/VertretungsplanApp3/VertretungsplanApp3.js
@@ -4,11 +4,28 @@ if (typeof kotlin === 'undefined') {
 var VertretungsplanApp3 = function (_, Kotlin) {
   'use strict';
   var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var removeClass = Kotlin.kotlin.dom.removeClass_hhb33f$;
   function main(args) {
-    var tmp$, tmp$_0;
+    var tmp$, tmp$_0, tmp$_1, tmp$_2;
     console.log('test');
     var vcardsContent = ((tmp$_0 = (tmp$ = document.getElementsByClassName('ly-page-vplan')[0]) != null ? tmp$.getElementsByClassName('ly-template-vcard') : null) != null ? tmp$_0 : Kotlin.throwNPE())[0];
-    println(vcardsContent);
+    var mainContainer = ((tmp$_2 = (tmp$_1 = document.getElementsByClassName('ly-page-vplan')[0]) != null ? tmp$_1.getElementsByClassName('vp-main-container') : null) != null ? tmp$_2 : Kotlin.throwNPE())[0];
+    if (Kotlin.isType(mainContainer, Element)) {
+      for (var i = 0; i < 10; i++) {
+        println('round: ' + i);
+        var newCard = vcardsContent != null ? vcardsContent.cloneNode(true) : null;
+        if (Kotlin.isType(newCard, Element)) {
+          println('is element');
+          newCard.removeAttribute('hidden');
+          removeClass(newCard, ['ly-template-vcard']);
+          var txClass = newCard.getElementsByClassName('vp-class')[0];
+          txClass != null ? (txClass.innerHTML = 'Klasse' + i) : null;
+          var txHours = newCard.getElementsByClassName('vp-hours')[0];
+          txHours != null ? (txHours.innerHTML = i.toString()) : null;
+          mainContainer.appendChild(newCard);
+        }
+      }
+    }
   }
   _.main_kand9s$ = main;
   main([]);
